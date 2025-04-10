60c48fc46306f927616aa27d90e0b8719606cfd8
mominur-helios <mominur.rahman@heliosinsider.com>
2025-04-10 22:43:24 +0600

    Refactor code and add jazzmin

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 def index(request):
 
     # Page from the theme 
-    return render(request, 'pages/dashboard.html')
+    return render(request, 'pages/dashboard.html', {'segment': 'dashboard'})
